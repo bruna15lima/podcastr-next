6fab605096dee845ed47ff31b78c5ab303adbbd3
bruna15lima <calheiros.bruna@gmail.com>
2021-04-21 21:01:01 -0300

    all

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,13 @@
+import format from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import styles from './styles.module.scss';
+
+export function Player() {
+  
+  return(
+    <div>
+      
+    </div>
+  )
+}
